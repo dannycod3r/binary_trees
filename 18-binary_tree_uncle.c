77c7fbdd2883b3935381cdc090981b0ae17a34e1
dannycod3r <oseid7807@gmail.com>
2023-11-02 18:12:02 +0000

    Add funtions to check sibling and uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - finds the uncle of a node in a binary tree
+ * @node: node to find uncle for
+ *
+ * Return: pointer to uncle node
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node)
+		return (NULL);
+	return (binary_tree_sibling(node->parent));
+}
+
+/**
+ * binary_tree_sibling - A function that finds the sibling of a node.
+ * @node: The node to find sibling for.
+ * Return: A pointer to sibling node.
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	/**/
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
+}
