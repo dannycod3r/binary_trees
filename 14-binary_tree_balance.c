5dfd3d9fa798c83c24447be2cb784a210bd706a9
dannycod3r <oseid7807@gmail.com>
2023-11-02 14:23:07 +0000

    Add custom height function that set height of empty tree to -1

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,7 +7,7 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+	size_t left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
@@ -15,5 +15,28 @@ int binary_tree_balance(const binary_tree_t *tree)
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	return (left_height - right_height);
+	return ((int)(left_height - right_height));
+}
+
+
+/**
+ * binary_tree_height - Calculate the height of a binary tree.
+ * @tree: A pointer to the root node of the binary tree.
+ *
+ * Return: The height of the binary tree, or 0 if the tree is NULL.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return -1; // Height of an empty tree is -1
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return left_height + 1;
+	else
+		return right_height + 1;
 }
