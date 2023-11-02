508033a4a5d08a0511281dfa6d158dfcf02524e4
dannycod3r <oseid7807@gmail.com>
2023-11-02 08:22:46 +0000

    Add check if node is leaf node

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include <stddef.h>
 
 /**
  * binary_tree_height - function that measures the height of btree
@@ -9,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
-	if (tree == NULL)
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
 	/*find height of left subtree*/
@@ -18,7 +19,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	/*find height of right subtree*/
 	right_height = binary_tree_height(tree->right);
 
-	/*return the maximum of the 2 plus root node*/
+	/*return the maximum of them*/
 	if (left_height > right_height)
 		return (left_height + 1);
 	else
