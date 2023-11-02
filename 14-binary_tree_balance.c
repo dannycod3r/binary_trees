e45955085a76d112cd3af6eac37e2f2c880a2d48
dannycod3r <oseid7807@gmail.com>
2023-11-02 14:36:43 +0000

    Fix betty

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -30,13 +30,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height, right_height;
 
 	if (tree == NULL)
-		return -1; // Height of an empty tree is -1
+		return (-1);
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
 	if (left_height > right_height)
-		return left_height + 1;
+		return (left_height + 1);
 	else
-		return right_height + 1;
+		return (right_height + 1);
 }
